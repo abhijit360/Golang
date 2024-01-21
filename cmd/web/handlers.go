4c8738005cc544b2197b6765c3ceeaada15c3c44
abhijit <kamathabhijit2002@gmail.com>
2024-01-20 23:09:37 -0500

    made progress on understanding logging information

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -3,12 +3,11 @@ package main
 import (
 	"fmt"
 	"html/template"
-	"log"
 	"net/http"
 	"strconv"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+func (app *application) home (w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		http.NotFound(w, r)
 		return
@@ -25,7 +24,7 @@ func home(w http.ResponseWriter, r *http.Request) {
 	ts, err := template.ParseFiles(files...)
 
 	if err != nil{
-		log.Print(err.Error())
+		app.logger.Error(err.Error(), "method", r.Method, "uri",r.URL.RequestURI())
 		http.Error(w, "INternal Server Error", http.StatusInternalServerError)
 		return
 	}
@@ -33,12 +32,12 @@ func home(w http.ResponseWriter, r *http.Request) {
 	// executeTemplate() method to write the content of base
 	err = ts.ExecuteTemplate(w,"base",nil)
 	if err != nil {
-		log.Print(err.Error())
+		app.logger.Error(err.Error(), "method", r.Method, "uri" , r.URL.RequestURI())
 		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 		}
 		
 }
-func snippetView(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	id, err := strconv.Atoi(r.URL.Query().Get("id"))
 	if err != nil || id < 1 {
 		http.NotFound(w, r)
@@ -46,7 +45,7 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Fprintf(w, "Display a specific snippet with ID %d...", id)
 }
-func snippetCreate(w http.ResponseWriter, r *http.Request) {
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow", http.MethodPost)
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,48 @@
 package main
 
 import (
-	"log"
+	"flag"
+	"log/slog"
 	"net/http"
+	"os"
 )
 
+
+// enables the dependencies for the application to be injected into the desired files
+// build handlers against this struct
+type application struct {
+	logger *slog.Logger
+}
+
 func main() {
+
+	// type config struct {
+	// 	addr string
+	// 	staticDir string
+	// }
+
+	// var cfg config
+	// //  to store the values into an existing variable using pointers
+	// flag.StringVar(&cfg.addr, "addr", ":4000", "HTTP Network Address")
+	// flag.StringVar(&cfg.staticDir, "static-dir", "./ui/static", "path to static assets")
+	
+	// define a command-line flag named addr with a default value
+	// flag will be stored in the addr variable at runtime
+	// the message will show when you add the "-help" flag when calling go run
+	addr := flag.String("addr", ":4000", "HTPP network Address")
+
+	// gets the addr variable from the command prompt
+	flag.Parse() 
+		
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+		AddSource: true,
+	}))
+
+	app := &application{
+		logger: logger,
+	}
+
 	mux := http.NewServeMux()
 
 	// create a file server which serves files out of the "./ui/static" directory
@@ -15,10 +52,11 @@ func main() {
 	mux.Handle("/static/", http.StripPrefix("/static",fileServer))
 
 
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
-	log.Print("starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
+	app.logger.Info("starting server on", "addr",*addr)
+	err := http.ListenAndServe(*addr, mux)
+	app.logger.Error(err.Error())
+	os.Exit(1)
 }
